0174c51c6bc1930316f6c697e3404de2049cd111
Silver <Silverg33k@gmail.com>
2023-01-06 21:51:36 +0100

    FEAT: Added user information retriever

--- src/interfaces/knex.interface.ts
+++ src/interfaces/knex.interface.ts
@@ -1,7 +1,7 @@
 export interface UserInterface {
     email: string;
     name: string;
-    password: string;
+    password?: string;
     phone_number: string;
     wallet?: number;
     account_type: string;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthRoutes } from './auth.route.js';
 import { LoansRoutes } from './loans.route.js';
 import { TransactionRoutes } from './transaction.route.js';
+import { UserRoutes } from './user.route.js';
 
 const router: Router = Router();
 
@@ -9,5 +10,6 @@ const router: Router = Router();
 router.use('/auth', AuthRoutes);
 router.use('/transaction', TransactionRoutes);
 router.use('/loans', LoansRoutes);
+router.use('/user', UserRoutes);
 
 export const AllRoutes: Router = router;
